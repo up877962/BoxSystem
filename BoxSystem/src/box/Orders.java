fc63c250f895199a315dc4f6a5dc6f2a285ce9ef
up878976 <up878976@W0LB6DEF131A6B8.uni.ds.port.ac.uk>
2018-11-22 15:50:13 +0000

    Modify orders

--- BoxSystem/src/box/Orders.java
+++ BoxSystem/src/box/Orders.java
@@ -14,4 +14,9 @@ public class Orders {
     public void addOrder(Order order){
         orders.add(order);
     }
+    public void checkOrder(int quantity, int grade, int colours, float height, float width, float depth, boolean sealableTop, boolean reinforcedBottom, boolean reinforcedCorner){
+        if((grade >=2 || grade<=5) && colours == 2 && reinforcedBottom && reinforcedCorner ){
+            addOrder(new Order(quantity,  grade,  height, width, depth, sealableTop, reinforcedBottom, reinforcedCorner));
+        }
+    }
 }
